aa5f912b9c6f0a0d19d5d66646c5b6be09535921
Kamrul Hasan <42377369+kaamrulhaasaan@users.noreply.github.com>
2018-09-17 13:15:24 +0600

    Add files via upload

---
+++ ArrayOfStrings.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+/*
+ * Author: Kamrul Hasan
+ * CSE 13th batch,PSTU
+ */
+
+public class ArrayOfStrings {
+
+	public static void main(String[] args) {
+		/*this is a simple java program to understand stings
+		 * of array
+		 */
+		
+		//declare String type array first
+		String[] texts = new String[3];
+		
+		texts[0] = "Hello";
+		texts[1] = "Mr. or Mrs";
+		texts[2] = "X";
+		
+		System.out.println(texts[0]);
+		System.out.println(texts[1]);
+		System.out.println(texts[2]);
+		
+	}
+
+}
